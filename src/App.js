a9737aa60dc21a476febd6d30aef2d07d12ced7e
Yohan Tahir <yohantahir@gmail.com>
2021-03-11 12:15:12 +0100

    create component navbar and quote

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-
+import Navbar from './components/Navbar';
+import QuoteCard from './components/QuoteCard';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <QuoteCard />
+      <QuoteCard />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import '../components/Navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+            <a href="https://fr.wikipedia.org/wiki/Les_Simpson">Simpsons quotes</a>
+        </nav>
+    )
+}
+
+export default Navbar;
